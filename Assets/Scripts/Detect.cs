1d805d9a116fc6f366ed37bbfd57ff884f186535
RBasic <rcham.pro@gmail.com>
2016-12-05 18:16:49 +0100

    changement repository
    nouveau repository projet taupe

---
+++ Assets/Scripts/Detect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Detect : MonoBehaviour
+{
+
+    public GameObject trace;
+    void Update()
+    {
+        GameObject go = Instantiate(trace);
+        go.transform.position = this.transform.position;
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(col.gameObject.name);
+    }
+}
---
+++ Assets/Scripts/t.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using Leap;
+
+public class t : MonoBehaviour
+{
+    public Controller c;
+
+    // Use this for initialization
+    void Start()
+    {
+        c = new Controller();
+        Debug.Log("c : "+c);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (c.IsConnected)
+        { //controller is a Controller object
+            Frame frame = c.Frame(); //The latest frame
+            if (frame.Hands.Count > 0)
+            {
+                List<Hand> hands = frame.Hands;
+                Hand firstHand = hands[0];
+                Debug.Log(firstHand);
+                Debug.Log(firstHand.GrabStrength);
+                if(firstHand.GrabStrength > 0.5f)
+                {
+                }
+                else
+                {
+                }
+            }
+        }
+
+
+
+    }
+
+}
